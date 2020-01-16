637c2ee28377bf6a6d57c39e3b66146e4e7e0342
vodle <thexhbik@gmail.com>
2020-01-16 03:10:50 +0300

    added info message

--- test_bot.py
+++ test_bot.py
@@ -6,8 +6,8 @@ import requests
 import urllib.request
 import random
 import sys
-
-token = 'NjAzNzE1NDY4MTA4NDk2OTEy.XhDuJA.kS1z41nLgWUEJkvNFhri9Y9j32Y'
+#science-fiction
+token = 'NjAzNzE1NDY4MTA4NDk2OTEy.XhOZfg.KYl0JjwKP_j7wl_ySiK9AKg_ia0'
 
 
 client = discord.Client()
@@ -30,23 +30,21 @@ async def on_message(message):
     author = message.author
     msgc = message.content
     msgg = message.channel
-
     if (str(msgg) == "science-fiction") and (msgc == "<!!dump!!>"):
+#scan and download pictures
         counter = 0
-        _msg = await message.channel.fetch_message('663353270940598272')
-
+        #_msg = await message.channel.fetch_message('663353270940598272')
         guild = "science-fiction"
-        async for elem in message.channel.history(limit=1500, oldest_first=True, after=_msg):
-            destination = 'F:\\code\\python\\bot\\pic\\' + 'pic' + str(random.randint(1, sys.maxsize)) + '.png'
+        start_time = time.ctime()
+        startTimeSecond = time.time()
+        async for elem in message.channel.history(limit=20000, oldest_first=True, after=None):
+            destination = 'F:\\code\\python\\bot\\pic\\' + 'pic' + str(counter) + '.png'
             if (len(elem.attachments) > 0):
 
                 for i in range(len(elem.attachments)):
                     _attachments = elem.attachments[i]
                     image = _attachments.url
                     print(image)
-
-
-
                     with open(destination, 'wb') as handle:
                         print(destination)
                         response = requests.get(image, stream=True)
@@ -54,9 +52,16 @@ async def on_message(message):
                             print(response)
                         for block in response.iter_content(1024):
                             if not block:
-                                    break
+                                break
                             handle.write(block)
                     handle.close()
                 counter += 1
+        await message.channel.send("dumped successful\ndump start at:" + start_time + "\n" + "dump end at:" + time.ctime() + "\ndump take: " + str(int(float(time.time()) - float(startTimeSecond))) + " seconds\n" )
+    if (str(msgg) == "bot-test") and (msgc == "info"):
+        print(message.guild)
+
+
+
+
 
 client.run(token)
